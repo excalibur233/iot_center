36f52a8f297e08e09f2437269452b7ae9be8d3eb
ming3000 <xuming.js@outlook.com>
2017-12-04 13:13:23 +0800

    test route

--- app/routes/test.route.js
+++ app/routes/test.route.js
@@ -3,7 +3,7 @@ const testController = require('../controllers/test.controller');
 
 router.get('/index', testController.index);
 
-const prefix = 'test';
+const prefix = '/test';
 module.exports = function (app) {
   app.use(prefix, router);
 };
