06ea7d8c53082d07455678e6656317d0e932276c
ming3000 <xuming.js@outlook.com>
2017-12-04 11:06:50 +0800

    config

--- iotCenter.js
+++ iotCenter.js
@@ -1,9 +1,9 @@
-process.enc.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.TZ = 'Asia/Shanghai';
 
-const configuration = require('./config/config');
+const config = require('./config/config');
 const app = require('./config/express');
 
-app.listen(configuration.port, function () {
-  console.log(`Server running at $(configuration.host):$(configuration.port)`);
+app.listen(config.port, function () {
+  console.log(`Server running at $(config.host):$(config.port)`);
 });
