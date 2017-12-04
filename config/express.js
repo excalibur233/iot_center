179eff43cc6ba0b81d24599da46af7fe1a031c98
ming3000 <xuming.js@outlook.com>
2017-12-04 11:45:18 +0800

    express

--- config/express.js
+++ config/express.js
@@ -1,3 +1,33 @@
-/**
- * Created by xumin on 2017/12/4.
- */
+const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+const logger = require('morgan');
+const path = require('path');
+
+module.exports = function () {
+  "use strict";
+  const app = express();
+
+  // use logger when development
+  if (process.env.NODE_ENV === 'development') {
+    app.use(logger('dev'));
+  }
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // register local routes here
+
+  // catch the 404 error.
+  app.use((req, res) => {
+    res.status(404);
+    res.json({error_code: '404'});
+  });
+
+  // catch the 500 error,
+  app.use((err, req, res, next) => {
+    res.status(500);
+    res.json({error_code: '500'});
+  });
+
+  return app;
+};
